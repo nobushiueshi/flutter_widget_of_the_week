8a8aa6ec63d03b125abc9c89544241001ed9691b
nobushiueshi <nobushiueshi@gmail.com>
2021-12-11 15:56:01 +0900

    #006 FutureBuilderの追加. フォルダ名の変更.

---
+++ lib/presentation/pages/sample000/sample_page_006.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class SamplePage006 extends StatefulWidget {
+  const SamplePage006({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _SamplePage006State createState() => _SamplePage006State();
+}
+
+class _SamplePage006State extends State<SamplePage006> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FutureBuilder'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('FutureBuilder Success'),
+              FutureBuilder(
+                future: _successFuture(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasError) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.info,
+                            color: Colors.red,
+                          ),
+                          Text(snapshot.error?.toString() ?? ''),
+                        ],
+                      );
+                    }
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.check_circle,
+                          color: Colors.green,
+                        ),
+                        Text(snapshot.data?.toString() ?? ''),
+                      ],
+                    );
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              const Text('FutureBuilder Error'),
+              FutureBuilder(
+                future: _failureFuture(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasError) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.info,
+                            color: Colors.red,
+                          ),
+                          Text(snapshot.error?.toString() ?? ''),
+                        ],
+                      );
+                    }
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.check_circle,
+                          color: Colors.green,
+                        ),
+                        Text(snapshot.data?.toString() ?? ''),
+                      ],
+                    );
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<String> _successFuture() async {
+    await Future.delayed(const Duration(seconds: 2), () {});
+    return 'Success!!';
+  }
+
+  Future<String> _failureFuture() async {
+    await Future.delayed(const Duration(seconds: 2), () {});
+
+    throw Exception('Error');
+  }
+}
--- lib/presentation/pages/sample_page_list.dart
+++ lib/presentation/pages/sample_page_list.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widget_of_the_week/presentation/pages/sample010/sample_page_001.dart';
-import 'package:flutter_widget_of_the_week/presentation/pages/sample010/sample_page_002.dart';
-import 'package:flutter_widget_of_the_week/presentation/pages/sample010/sample_page_003.dart';
-import 'package:flutter_widget_of_the_week/presentation/pages/sample010/sample_page_004.dart';
-import 'package:flutter_widget_of_the_week/presentation/pages/sample010/sample_page_005.dart';
+import 'package:flutter_widget_of_the_week/presentation/pages/sample000/sample_page_001.dart';
+import 'package:flutter_widget_of_the_week/presentation/pages/sample000/sample_page_002.dart';
+import 'package:flutter_widget_of_the_week/presentation/pages/sample000/sample_page_003.dart';
+import 'package:flutter_widget_of_the_week/presentation/pages/sample000/sample_page_004.dart';
+import 'package:flutter_widget_of_the_week/presentation/pages/sample000/sample_page_005.dart';
+import 'package:flutter_widget_of_the_week/presentation/pages/sample000/sample_page_006.dart';
 
 class SamplePageData {
   SamplePageData({
@@ -16,6 +17,7 @@ class SamplePageData {
 }
 
 final samplePageDataList = <SamplePageData>[
+  SamplePageData(name: '#6 FutureBuilder', widget: const SamplePage006()),
   SamplePageData(name: '#5 Opacity', widget: const SamplePage005()),
   SamplePageData(name: '#4 AnimatedContainer', widget: const SamplePage004()),
   SamplePageData(name: '#3 Wrap', widget: const SamplePage003()),
