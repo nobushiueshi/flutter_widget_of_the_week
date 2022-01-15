4c5926fa007224dc5663025d19809dc16604cb7d
nobushiueshi <nobushiueshi@gmail.com>
2022-01-15 22:28:01 +0900

    #020 FittedBoxを追加

---
+++ lib/presentation/pages/sample/sample020_page_data_list.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_widget_of_the_week/presentation/pages/sample/sample_page_data.dart';
+import 'package:flutter_widget_of_the_week/presentation/pages/sample020/sample_page_020.dart';
+
+final sample020PageDataList = <SamplePageData>[
+  SamplePageData(name: '#20 FittedBox', widget: const SamplePage020()),
+];
---
+++ lib/presentation/pages/sample020/sample_page_020.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class SamplePage020 extends StatelessWidget {
+  const SamplePage020({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FittedBox'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Center(
+              child: Text('FittedBoxなし'),
+            ),
+            Container(
+              width: 300,
+              height: 300,
+              color: Colors.grey,
+              child: const _SamplePage020Child(),
+            ),
+            const Center(
+              child: Text('FittedBox BoxFit.fill'),
+            ),
+            Container(
+              width: 300,
+              height: 300,
+              color: Colors.grey,
+              child: const FittedBox(
+                fit: BoxFit.fill,
+                child: _SamplePage020Child(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SamplePage020Child extends StatelessWidget {
+  const _SamplePage020Child();
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/sample.png',
+    );
+  }
+}
