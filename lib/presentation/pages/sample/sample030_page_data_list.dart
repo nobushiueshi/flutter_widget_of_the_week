2216e62e5db9d95d0533d886c9a071214a749ed8
nobushiueshi <nobushiueshi@gmail.com>
2022-03-21 17:15:31 +0900

    #34 MediaQueryを追加.

--- lib/presentation/pages/sample/sample030_page_data_list.dart
+++ lib/presentation/pages/sample/sample030_page_data_list.dart
@@ -3,8 +3,10 @@ import 'package:flutter_widget_of_the_week/presentation/pages/sample030/sample_p
 import 'package:flutter_widget_of_the_week/presentation/pages/sample030/sample_page_031.dart';
 import 'package:flutter_widget_of_the_week/presentation/pages/sample030/sample_page_032.dart';
 import 'package:flutter_widget_of_the_week/presentation/pages/sample030/sample_page_033.dart';
+import 'package:flutter_widget_of_the_week/presentation/pages/sample030/sample_page_034.dart';
 
 final sample030PageDataList = <SamplePageData>[
+  SamplePageData(name: '#34 MediaQuery', widget: const SamplePage034()),
   SamplePageData(name: '#33 Flexible', widget: const SamplePage033()),
   SamplePageData(name: '#32 AnimatedList', widget: const SamplePage032()),
   SamplePageData(name: '#31 Draggable', widget: const SamplePage031()),
---
+++ lib/presentation/pages/sample030/sample_page_034.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class SamplePage034 extends StatefulWidget {
+  const SamplePage034({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SamplePage034> createState() => _SamplePage034State();
+}
+
+class _SamplePage034State extends State<SamplePage034> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final orientation = MediaQuery.of(context).orientation;
+    final textScaleFactor = MediaQuery.of(context).textScaleFactor;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MediaQuery'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Text('size : $size'),
+              Text('orientation : $orientation'),
+              Text('textScaleFactor : $textScaleFactor'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
