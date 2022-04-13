f45051b12ebd29357e215dcb89a6b01ee7ec21e5
nobushiueshi <nobushiueshi@gmail.com>
2022-04-13 22:50:49 +0900

    #046 IndexedStackを追加.

--- lib/presentation/pages/sample/sample040_page_data_list.dart
+++ lib/presentation/pages/sample/sample040_page_data_list.dart
@@ -5,8 +5,10 @@ import 'package:flutter_widget_of_the_week/presentation/pages/sample040/sample_p
 import 'package:flutter_widget_of_the_week/presentation/pages/sample040/sample_page_043.dart';
 import 'package:flutter_widget_of_the_week/presentation/pages/sample040/sample_page_044.dart';
 import 'package:flutter_widget_of_the_week/presentation/pages/sample040/sample_page_045.dart';
+import 'package:flutter_widget_of_the_week/presentation/pages/sample040/sample_page_046.dart';
 
 final sample040PageDataList = <SamplePageData>[
+  SamplePageData(name: '#46 IndexedStack', widget: const SamplePage046()),
   SamplePageData(name: '#45 AnimatedPadding', widget: const SamplePage045()),
   SamplePageData(name: '#44 AnimatedPositioned', widget: const SamplePage044()),
   SamplePageData(name: '#43 AnimatedSwitcher', widget: const SamplePage043()),
---
+++ lib/presentation/pages/sample040/sample_page_046.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class SamplePage046 extends StatefulWidget {
+  const SamplePage046({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SamplePage046> createState() => _SamplePage046State();
+}
+
+class _SamplePage046State extends State<SamplePage046> {
+  var _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('IndexedStack'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: IndexedStack(
+          index: _currentIndex,
+          children: const [
+            _SamplePage046Child(
+              text: 'Home',
+            ),
+            _SamplePage046Child(
+              text: 'Settings',
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class _SamplePage046Child extends StatefulWidget {
+  const _SamplePage046Child({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  State<_SamplePage046Child> createState() => _SamplePage046ChildState();
+}
+
+class _SamplePage046ChildState extends State<_SamplePage046Child> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                widget.text,
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        heroTag: widget.text,
+        child: const Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            _counter += 1;
+          });
+        },
+      ),
+    );
+  }
+}
