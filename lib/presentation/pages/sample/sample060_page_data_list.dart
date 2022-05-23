30cd8cd899a274808fd82774d8f46fc50d2f397a
nobushiueshi <nobushiueshi@gmail.com>
2022-05-23 22:38:19 +0900

    #063 CupertinoActionSheetを追加.

--- lib/presentation/pages/sample/sample060_page_data_list.dart
+++ lib/presentation/pages/sample/sample060_page_data_list.dart
@@ -2,8 +2,13 @@ import 'package:flutter_widget_of_the_week/presentation/pages/sample/sample_page
 import 'package:flutter_widget_of_the_week/presentation/pages/sample060/sample_page_060.dart';
 import 'package:flutter_widget_of_the_week/presentation/pages/sample060/sample_page_061.dart';
 import 'package:flutter_widget_of_the_week/presentation/pages/sample060/sample_page_062.dart';
+import 'package:flutter_widget_of_the_week/presentation/pages/sample060/sample_page_063.dart';
 
 final sample060PageDataList = <SamplePageData>[
+  SamplePageData(
+    name: '#63 CupertinoActionSheet',
+    widget: const SamplePage063(),
+  ),
   SamplePageData(name: '#62 ToggleButtons', widget: const SamplePage062()),
   SamplePageData(name: '#61 ColorFiltered', widget: const SamplePage061()),
   SamplePageData(
---
+++ lib/presentation/pages/sample060/sample_page_063.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class SamplePage063 extends StatefulWidget {
+  const SamplePage063({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SamplePage063> createState() => _SamplePage063State();
+}
+
+class _SamplePage063State extends State<SamplePage063> {
+  String _text = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CupertinoActionSheet'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Text(_text),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final text = await showCupertinoModalPopup<String>(
+            context: context,
+            builder: (context) {
+              return CupertinoActionSheet(
+                title: const Text('タイトル'),
+                message: const Text('メッセージ'),
+                actions: [
+                  CupertinoActionSheetAction(
+                    onPressed: () {
+                      Navigator.of(context).pop('Hello');
+                    },
+                    isDefaultAction: true,
+                    child: const Text('Hello'),
+                  ),
+                  CupertinoActionSheetAction(
+                    onPressed: () {
+                      Navigator.of(context).pop('Goodbye');
+                    },
+                    child: const Text('Goodbye'),
+                  )
+                ],
+                cancelButton: CupertinoActionSheetAction(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  isDestructiveAction: true,
+                  child: const Text('キャンセル'),
+                ),
+              );
+            },
+          );
+          if (text == null) {
+            return;
+          }
+          setState(() {
+            _text = text;
+          });
+        },
+        child: const Icon(Icons.open_in_new),
+      ),
+    );
+  }
+}
