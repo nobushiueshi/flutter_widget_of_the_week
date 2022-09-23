1d84405a874e10d27b0162f1adad200c0d2d8594
nobushiueshi <nobushiueshi@gmail.com>
2022-09-23 15:59:45 +0900

    #110 TabPageSelectorを追加.

---
+++ lib/presentation/pages/sample/sample110_page_data_list.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_widget_of_the_week/presentation/pages/sample/sample_page_data.dart';
+import 'package:flutter_widget_of_the_week/presentation/pages/sample110/sample_page_110.dart';
+
+final sample110PageDataList = <SamplePageData>[
+  SamplePageData(name: '#110 TabPageSelector', widget: const SamplePage110()),
+];
---
+++ lib/presentation/pages/sample110/sample_page_110.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class SamplePage110 extends StatefulWidget {
+  const SamplePage110({
+    super.key,
+  });
+
+  @override
+  State<SamplePage110> createState() => _SamplePage110State();
+}
+
+class _SamplePage110State extends State<SamplePage110>
+    with SingleTickerProviderStateMixin {
+  late final TabController tabController;
+
+  final icons = const <IconData>[
+    Icons.home,
+    Icons.drive_eta,
+    Icons.settings,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(
+      length: 3,
+      vsync: this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TabPageSelector'),
+        centerTitle: true,
+        bottom: TabBar(
+          controller: tabController,
+          tabs: icons
+              .map(
+                (icon) => Tab(
+                  child: Icon(icon),
+                ),
+              )
+              .toList(),
+        ),
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            TabBarView(
+              controller: tabController,
+              children: icons
+                  .map(
+                    (icon) => Center(
+                      child: Icon(
+                        icon,
+                        size: 128,
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: TabPageSelector(
+                  controller: tabController,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
