e6ba7dc33c5c28d7eb354b9e26f523c9ccfe3f26
nobushiueshi <nobushiueshi@gmail.com>
2023-01-16 18:24:39 +0900

    コミット漏れ

--- lib/presentation/pages/sample130/sample_page_135.dart
+++ lib/presentation/pages/sample130/sample_page_135.dart
@@ -1,28 +1,28 @@
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
-import 'package:flutter/material.dart';
+// import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+// import 'package:flutter/material.dart';
 
-class SamplePage135 extends StatefulWidget {
-  const SamplePage135({
-    super.key,
-  });
+// class SamplePage135 extends StatefulWidget {
+//   const SamplePage135({
+//     super.key,
+//   });
 
-  @override
-  State<SamplePage135> createState() => _SamplePage135State();
-}
+//   @override
+//   State<SamplePage135> createState() => _SamplePage135State();
+// }
 
-class _SamplePage135State extends State<SamplePage135> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase UI for Auth',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      routes: {
-        '/': (context) => SignInScreen(
-              providers: [EmailAuthProvider()],
-            ),
-      },
-    );
-  }
-}
+// class _SamplePage135State extends State<SamplePage135> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Firebase UI for Auth',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       routes: {
+//         '/': (context) => SignInScreen(
+//               providers: [EmailAuthProvider()],
+//             ),
+//       },
+//     );
+//   }
+// }
