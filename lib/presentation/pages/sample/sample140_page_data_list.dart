37835da7761ace62e7accb6c8b48a90efb636aae
nobushiueshi <26820149+nobushiueshi@users.noreply.github.com>
2024-05-14 23:42:52 +0900

    #144 flutter_animate

--- lib/presentation/pages/sample/sample140_page_data_list.dart
+++ lib/presentation/pages/sample/sample140_page_data_list.dart
@@ -1,8 +1,14 @@
 import 'package:flutter_widget_of_the_week/presentation/pages/sample/sample_page_data.dart';
 import 'package:flutter_widget_of_the_week/presentation/pages/sample140/sample_page_141.dart';
 // import 'package:flutter_widget_of_the_week/presentation/pages/sample140/sample_page_143.dart';
+import 'package:flutter_widget_of_the_week/presentation/pages/sample140/sample_page_144.dart';
 
 final sample140PageDataList = <SamplePageData>[
+  SamplePageData(
+    name: '#144 flutter_animate',
+    contentType: SamplePageContentType.package,
+    widget: const SamplePage144(),
+  ),
   SamplePageData(
     name: '#143 firebase_storage',
     contentType: SamplePageContentType.package,
---
+++ lib/presentation/pages/sample140/sample_page_144.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+class SamplePage144 extends StatefulWidget {
+  const SamplePage144({
+    super.key,
+  });
+
+  @override
+  State<SamplePage144> createState() => _SamplePage144State();
+}
+
+class _SamplePage144State extends State<SamplePage144> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('flutter_animate'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const Text('Hello World!').animate().fade().scale(),
+              const SizedBox(height: 20),
+              Column(
+                children: [
+                  const Text('Hello'),
+                  const Text('World'),
+                  const Text('Goodbye'),
+                ].animate(interval: 400.ms).fade(duration: 300.ms),
+              ),
+              const SizedBox(height: 20),
+              const Text('Before').animate().swap(
+                    duration: 900.ms,
+                    builder: (_, __) => const Text('After'),
+                  ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
