ab0671ffb79c4beb1c9b6abad35bce8ae825b7f9
nobushiueshi <26820149+nobushiueshi@users.noreply.github.com>
2024-06-12 22:55:41 +0900

    #150 home_widget

---
+++ lib/presentation/pages/sample/sample150_page_data_list.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_widget_of_the_week/presentation/pages/sample/sample_page_data.dart';
+
+final sample150PageDataList = <SamplePageData>[
+  SamplePageData(
+    name: '#150 home_widget',
+    contentType: SamplePageContentType.package,
+  ),
+];
